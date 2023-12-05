0cf7c8051d035cea1eb9f9dbb6d3a797ccfc33f6
brian <aiephoenixbl@gmail.com>
2023-12-05 18:22:44 +0800

    feat: complete ServerTaskBundleScene type.

--- src/types/api-router/server/task-bundle/Scene.ts
+++ src/types/api-router/server/task-bundle/Scene.ts
@@ -1,1 +1,10 @@
-export interface ServerTaskBundleScene {}
+export interface ServerTaskBundleScene {
+  config: {
+    scene_info: Record<string, any>;
+    scene_agents: Record<string, any>;
+
+    [key: string]: any;
+  };
+
+  [key: string]: any;
+}
