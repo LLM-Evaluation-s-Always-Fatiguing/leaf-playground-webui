a9731fe365cf3146e00fb761958242889a6c10a5
brian <aiephoenixbl@gmail.com>
2024-01-22 16:32:39 +0800

    feat: adjust UploadStringFileContent style

--- src/components/formily/components/UploadStringFileContent.tsx
+++ src/components/formily/components/UploadStringFileContent.tsx
@@ -10,49 +10,51 @@ interface FormilyUploadStringFileContentProps {
 
 const FormilyUploadStringFileContent = (props: FormilyUploadStringFileContentProps) => {
   return (
-    <Dragger
-      fileList={
-        props.value
-          ? [
+    <div>
+      <Dragger
+        fileList={
+          props.value
+            ? [
               {
                 name: 'Current File',
               } as any,
             ]
-          : []
-      }
-      beforeUpload={async (file) => {
-        try {
-          const result = await new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.onload = () => resolve(reader.result);
-            reader.onerror = (e) => reject(e);
-            reader.readAsText(file);
-          });
-          if (result) {
-            props.onChange(result as string);
-          }
-        } catch (e) {
-          console.error(e);
-          message.error('Failed to read file as text!');
+            : []
         }
-        return false;
-      }}
-      onRemove={() => {
-        props.onChange(undefined);
-      }}
-    >
-      <p className="ant-upload-drag-icon">
-        <InboxOutlined />
-      </p>
-      <p className="ant-upload-text">Click or drag file to this area to upload</p>
-      {props.value && (
-        <p className="ant-upload-hint" style={{
-          fontSize: 16, fontWeight: 500
-        }}>
-          Current file content: {props.value.substring(0, Math.min(props.value.length, 88))}...
+        beforeUpload={async (file) => {
+          try {
+            const result = await new Promise((resolve, reject) => {
+              const reader = new FileReader();
+              reader.onload = () => resolve(reader.result);
+              reader.onerror = (e) => reject(e);
+              reader.readAsText(file);
+            });
+            if (result) {
+              props.onChange(result as string);
+            }
+          } catch (e) {
+            console.error(e);
+            message.error('Failed to read file as text!');
+          }
+          return false;
+        }}
+        onRemove={() => {
+          props.onChange(undefined);
+        }}
+      >
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
         </p>
-      )}
-    </Dragger>
+        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        {props.value && (
+          <p className="ant-upload-hint" style={{
+            fontSize: 16, fontWeight: 500
+          }}>
+            Current file content: {props.value.substring(0, Math.min(props.value.length, 88))}...
+          </p>
+        )}
+      </Dragger>
+    </div>
   );
 };
 
